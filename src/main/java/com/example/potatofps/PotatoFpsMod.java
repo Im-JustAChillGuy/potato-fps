bd093156a712a75f1458c9099c23de525b3e68f3
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-03-04 08:42:13 +0530

    Update PotatoFpsMod.java

--- src/main/java/com/example/potatofps/PotatoFpsMod.java
+++ src/main/java/com/example/potatofps/PotatoFpsMod.java
@@ -1,13 +1,11 @@
 package com.example.potatofps;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
 
 public class PotatoFpsMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        System.out.println("Potato FPS Mod Loaded!");
-        MinecraftClient client = MinecraftClient.getInstance();
+        System.out.println("Potato FPS mod loaded!");
     }
 }
