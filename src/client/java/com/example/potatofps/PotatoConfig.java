30dd6dbb1a3987190b1109721a496b50979c11a0
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-03-07 14:43:39 +0530

    Create PotatoConfig.java

---
+++ src/client/java/com/example/potatofps/PotatoConfig.java
@@ -0,0 +1,14 @@
+package com.example.potatofps;
+
+public class PotatoConfig {
+
+    public static boolean potatoMode = true;
+    public static boolean reduceParticles = true;
+    public static boolean disableShadows = true;
+    public static boolean disableClouds = true;
+    public static boolean disableVignette = true;
+    public static boolean ultraParticleReduction = false;
+    public static boolean reduceEntityDistance = true;
+    public static boolean disableRainParticles = true;
+
+}
