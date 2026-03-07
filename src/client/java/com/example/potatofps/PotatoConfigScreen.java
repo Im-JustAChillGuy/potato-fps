fa88c51d476a9edcf18ed3ba7f9e032aa06debea
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-03-07 14:41:58 +0530

    Update PotatoConfigScreen.java

--- src/client/java/com/example/potatofps/PotatoConfigScreen.java
+++ src/client/java/com/example/potatofps/PotatoConfigScreen.java
@@ -2,6 +2,7 @@ package com.example.potatofps;
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
@@ -14,12 +15,55 @@ public class PotatoConfigScreen {
                 .setTitle(Text.literal("Potato FPS Settings"));
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        ConfigCategory general = builder.getOrCreateCategory(Text.literal("Performance"));
 
-        builder.getOrCreateCategory(Text.literal("General"))
-                .addEntry(entryBuilder.startBooleanToggle(
-                        Text.literal("Enable Potato Mode"),
-                        true
-                ).setDefaultValue(true).build());
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Enable Potato Mode"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Reduce Particles"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Disable Entity Shadows"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Disable Clouds"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Disable Vignette"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Ultra Particle Reduction"), false)
+                        .setDefaultValue(false)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Reduce Entity Render Distance"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
+
+        general.addEntry(
+                entryBuilder.startBooleanToggle(Text.literal("Disable Rain Splash Particles"), true)
+                        .setDefaultValue(true)
+                        .build()
+        );
 
         return builder.build();
     }
