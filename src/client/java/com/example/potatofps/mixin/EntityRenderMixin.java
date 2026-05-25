1ad3577701ae3f6efe08f183cc57be6e7c259ae3
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-25 18:07:04 +0530

    Update EntityRenderMixin.java

--- src/client/java/com/example/potatofps/mixin/EntityRenderMixin.java
+++ src/client/java/com/example/potatofps/mixin/EntityRenderMixin.java
@@ -1,34 +1,46 @@
 package com.example.potatofps.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.entity.Entity;
-
 import com.example.potatofps.PotatoConfig;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EntityRenderer.class)
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net.minecraft.client.renderer.entity.EntityRenderer")
 public class EntityRenderMixin<T extends Entity> {
 
-    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
-    private void potatoFPS$cullEntities(T entity, CallbackInfo ci) {
+    @Inject(
+            method = "shouldRender",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void potatoFPS$cullEntities(
+            T entity,
+            CallbackInfoReturnable<Boolean> cir
+    ) {
 
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.player == null) return;
+        Minecraft client = Minecraft.getInstance();
 
-        if (PotatoConfig.skipInvisible && entity.isInvisible()) {
-            ci.cancel();
+        if (client.player == null) {
             return;
         }
 
-        double max = PotatoConfig.entityRenderDistance * PotatoConfig.entityRenderDistance;
+        if (PotatoConfig.skipInvisible && entity.isInvisible()) {
+            cir.setReturnValue(false);
+            return;
+        }
 
-        if (entity.squaredDistanceTo(client.player) > max) {
-            ci.cancel();
+        double max =
+                PotatoConfig.entityRenderDistance
+                * PotatoConfig.entityRenderDistance;
+
+        if (entity.distanceToSqr(client.player) > max) {
+            cir.setReturnValue(false);
         }
     }
 }
