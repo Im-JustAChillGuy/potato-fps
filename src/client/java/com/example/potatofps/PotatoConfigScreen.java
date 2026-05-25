6fff97102c8ed6517202d6ef83c15f597822bf96
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-25 18:06:08 +0530

    Update PotatoConfigScreen.java

--- src/client/java/com/example/potatofps/PotatoConfigScreen.java
+++ src/client/java/com/example/potatofps/PotatoConfigScreen.java
@@ -1,10 +1,11 @@
 package com.example.potatofps;
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 
 public class PotatoConfigScreen {
 
@@ -12,123 +13,161 @@ public class PotatoConfigScreen {
 
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent)
-                .setTitle(Text.literal("Potato FPS Settings"))
-                .setSavingRunnable(() -> PotatoConfigManager.save());
+                .setTitle(Component.literal("Potato FPS Settings"))
+                .setSavingRunnable(PotatoConfigManager::save);
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        ConfigCategory general = builder.getOrCreateCategory(Text.literal("Performance"));
+
+        ConfigCategory general = builder.getOrCreateCategory(
+                Component.literal("Performance")
+        );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Enable Potato Mode"), PotatoConfig.potatoMode)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Enable Potato Mode"),
+                                PotatoConfig.potatoMode
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.potatoMode = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.potatoMode = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Disable Entity Shadows"), PotatoConfig.disableShadows)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Disable Entity Shadows"),
+                                PotatoConfig.disableShadows
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.disableShadows = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.disableShadows = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Disable Clouds"), PotatoConfig.disableClouds)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Disable Clouds"),
+                                PotatoConfig.disableClouds
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.disableClouds = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.disableClouds = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Disable Vignette"), PotatoConfig.disableVignette)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Disable Vignette"),
+                                PotatoConfig.disableVignette
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.disableVignette = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.disableVignette = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Ultra Particle Reduction"), PotatoConfig.ultraParticleReduction)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Ultra Particle Reduction"),
+                                PotatoConfig.ultraParticleReduction
+                        )
                         .setDefaultValue(false)
-                        .setSaveConsumer(newValue -> PotatoConfig.ultraParticleReduction = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.ultraParticleReduction = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Reduce Entity Render Distance"), PotatoConfig.reduceEntityDistance)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Reduce Entity Render Distance"),
+                                PotatoConfig.reduceEntityDistance
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.reduceEntityDistance = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.reduceEntityDistance = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Disable Rain Splash Particles"), PotatoConfig.disableRainParticles)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Disable Rain Splash Particles"),
+                                PotatoConfig.disableRainParticles
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.disableRainParticles = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.disableRainParticles = value)
                         .build()
         );
 
         general.addEntry(
                 entryBuilder.startIntSlider(
-                        Text.literal("Minimum Render Distance"),
-                        PotatoConfig.minRender,
-                        2,
-                        32
-                )
+                                Component.literal("Minimum Render Distance"),
+                                PotatoConfig.minRender,
+                                2,
+                                32
+                        )
                         .setDefaultValue(8)
-                        .setSaveConsumer(newValue -> PotatoConfig.minRender = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.minRender = value)
                         .build()
         );
 
         general.addEntry(
                 entryBuilder.startIntSlider(
-                        Text.literal("Maximum Render Distance"),
-                        PotatoConfig.maxRender,
-                        4,
-                        32
-                )
+                                Component.literal("Maximum Render Distance"),
+                                PotatoConfig.maxRender,
+                                4,
+                                32
+                        )
                         .setDefaultValue(16)
-                        .setSaveConsumer(newValue -> PotatoConfig.maxRender = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.maxRender = value)
                         .build()
         );
 
         general.addEntry(
                 entryBuilder.startIntSlider(
-                        Text.literal("Target FPS"),
-                        PotatoConfig.targetFps,
-                        30,
-                        240
-                )
+                                Component.literal("Target FPS"),
+                                PotatoConfig.targetFps,
+                                30,
+                                240
+                        )
                         .setDefaultValue(70)
-                        .setSaveConsumer(newValue -> PotatoConfig.targetFps = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.targetFps = value)
                         .build()
         );
 
         general.addEntry(
-                entryBuilder.startBooleanToggle(Text.literal("Show Potato HUD"), PotatoConfig.showHud)
+                entryBuilder.startBooleanToggle(
+                                Component.literal("Show Potato HUD"),
+                                PotatoConfig.showHud
+                        )
                         .setDefaultValue(true)
-                        .setSaveConsumer(newValue -> PotatoConfig.showHud = newValue)
+                        .setSaveConsumer(value ->
+                                PotatoConfig.showHud = value)
                         .build()
         );
 
         general.addEntry(
                 entryBuilder.startEnumSelector(
-                        Text.literal("Adjustment Speed"),
-                        AdjustmentSpeed.class,
-                        AdjustmentSpeed.values()[PotatoConfig.adjustmentSpeed]
-                )
+                                Component.literal("Adjustment Speed"),
+                                AdjustmentSpeed.class,
+                                AdjustmentSpeed.values()[PotatoConfig.adjustmentSpeed]
+                        )
                         .setDefaultValue(AdjustmentSpeed.SLOW)
-                        .setSaveConsumer(newValue -> PotatoConfig.adjustmentSpeed = newValue.ordinal())
+                        .setSaveConsumer(value ->
+                                PotatoConfig.adjustmentSpeed = value.ordinal())
                         .build()
         );
 
-        // Fixed section (category -> general)
         general.addEntry(
                 entryBuilder.startIntSlider(
-                        Text.literal("Entity Render Distance"),
-                        PotatoConfig.entityRenderDistance,
-                        16,
-                        128
-                )
+                                Component.literal("Entity Render Distance"),
+                                PotatoConfig.entityRenderDistance,
+                                16,
+                                128
+                        )
                         .setDefaultValue(64)
                         .setSaveConsumer(value -> {
                             PotatoConfig.entityRenderDistance = value;
